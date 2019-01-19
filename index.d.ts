844f919c51826b050d904ade784b4151da06f201
Volkan Nazmi Metin <volkan.nazmi.metin@gmail.com>
2019-01-20 01:44:11 +0300

    Better type definitions

--- index.d.ts
+++ index.d.ts
@@ -2,16 +2,18 @@
 // Project: argument-required
 // Definitions by: Volkan Nazmi Metin https://github.com/volem
 
-export as namespace required
+/**
+ * Throws an error if argument array doesn't contain truthy values in the order given by argumentNames
+ */
+export interface ArgumentsFunction {
+	/**
+	 * @param argumentArray Any type of arguments that needs to be checked for existance
+	 */
+	(...argumentArray: any[]): void;
+}
 
 /**
  * Name of the arguments that needs to be checked in the given order.
  * @param argumentNames String array for the name of the arguments
  */
-export const required = (...argumentNames: string[]) =>
-	/**
-	 * Throws an error if argument array doesn't contain truthy values in the order given by argumentNames 
-	 * @param argumentArray Any type of arguments that needs to be checked for existance
-	 */
-	(...argumentArray: any[]) => {};
-
+export function required(...argumentNames: string[]) : ArgumentsFunction;
