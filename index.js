b1e96fa01a723077085962087ef36b82e445aeca
Volkan Metin <v.metin@papara.com>
2020-10-03 23:55:57 +0300

    Better empty argument handle

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = (...argumentName) => (...argumentArray) => {
 	argumentName.forEach(function (name, index) {
-		if (!argumentArray[index]) {
+		if (argumentArray[index] === undefined || argumentArray[index] === null || argumentArray[index] === '') {
 			const error = new Error(`Argument required : ${name}`);
 			error.name = 'ArgumentRequiredError';
 			throw error;
